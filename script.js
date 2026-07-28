c9702a829bbd958530906f80428556b8c3ca7d69
Udhaya Surya M <udhayasuryamurugan@gmail.com>
2026-07-28 15:20:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior:"smooth"
+        });
+    });
+});
